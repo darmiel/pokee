54a79f199b20335e78920f035a4bb71e70f0e3d9
darmiel <71837281+darmiel@users.noreply.github.com>
2024-04-21 12:37:23 +0200

    feat[psql]: initial commit

--- pokee-bootstrap/src/main/java/com/github/pokee/bootstrap/handlers/HelloWorldHandler.java
+++ pokee-bootstrap/src/main/java/com/github/pokee/bootstrap/handlers/HelloWorldHandler.java
@@ -12,6 +12,8 @@ import com.github.pokee.pswf.response.ResponseLike;
 import com.github.pokee.pswf.response.StatusCode;
 import com.github.pokee.pswf.util.ContentTypes;
 
+import java.util.List;
+
 @RoutePrefix("/hello")
 public class HelloWorldHandler {
 
@@ -24,6 +26,8 @@ public class HelloWorldHandler {
     public String sayHello() {
         return "Hello World!";
     }
+
+    ///
 
     /**
      * A simple hello world endpoint that serializes the response to JSON
@@ -38,6 +42,21 @@ public class HelloWorldHandler {
     }
 
     /**
+     * A simple hello world endpoint that serializes the response to JSON
+     *
+     * @return A simple hello world message.
+     */
+    @GET("/:name/multi")
+    public List<HelloResponse> sayHelloMulti(@Param("name") final String name) {
+        return List.of(new HelloResponse(name, 0), new HelloResponse(name, 0));
+    }
+
+    public record HelloResponse(@JsonProperty("Hello") String target, @JsonProperty("Age") int age) {
+    }
+
+    ///
+
+    /**
      * A simple hello world endpoint that returns a raw response
      *
      * @return A simple hello world message.
@@ -50,6 +69,8 @@ public class HelloWorldHandler {
                 .build();
     }
 
+    ///
+
     /**
      * A simple hello world endpoint that gets the response from {@link ResponseLike}
      *
@@ -58,9 +79,6 @@ public class HelloWorldHandler {
     @GET("/world")
     public HelloWorldResponse sayHelloWorld() {
         return new HelloWorldResponse();
-    }
-
-    public record HelloResponse(@JsonProperty("Hello") String target, @JsonProperty("Age") int age) {
     }
 
     public record HelloWorldResponse() implements ResponseLike {
---
+++ pokee-psql/src/main/java/com/github/pokee/psql/Psql.java
@@ -0,0 +1,9 @@
+package com.github.pokee.psql;
+
+public class Psql {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
